c568515c7e2c209afc13e92d3067983e30df37f6
reborn_star1 <rebornstar677@gmail.com>
2024-11-14 16:00:59 +0530

    Initial Setup for the Web Application

---
+++ client/src/App.js
@@ -0,0 +1,12 @@
+import logo from './logo.svg';
+import './App.css';
+
+function App() {
+  return (
+    <div className="App">
+      This is just the Random Code
+    </div>
+  );
+}
+
+export default App;
